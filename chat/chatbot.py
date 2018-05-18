d300b7746ec50d8138f29a487dc5bae0d304467d
rodneyzhang <409220663@qq.com>
2018-05-18 16:54:00 +0800

    Signed-off-by: rodneyzhang <409220663@qq.com>

--- chat/chatbot.py
+++ chat/chatbot.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, jsonify
-import os,sys,json
+import os,sys,json,socket
 sys.path.append("/data/app/chat/")
-from chat.client import match
+from chat.client import match,question_pack
+from chat.config import getConfig
 
 app = Flask(__name__)
 
@@ -12,12 +13,19 @@ def hello():
 @app.route("/ask", methods=['POST'])
 def ask():
     message = request.form['messageText'].strip()
+    
     if message == "quit":
         exit()
     else:
-        bot_response = match(question = message)
+        mysock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        mysock.connect(( getConfig("nluserver", "host"), int(getConfig("nluserver", "port")) ))
+        send = question_pack(message, userid = "A0001")
+        mysock.sendall(send.encode("UTF-8"))
+        received = mysock.recv(4096) # 2048->4096(2018-1-5 添加了 xml 后扩充)
+        received = received.decode("UTF-8")
+
         # print bot_response
-        return jsonify({'status':'OK','answer':json.loads(bot_response)['content']})
+        return jsonify({'status':'OK','answer':json.loads(received)['content']})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
